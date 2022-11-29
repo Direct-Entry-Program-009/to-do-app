18d7b5b8ea2e137eb5eaeaccc10b38633dee1c2b
naween1994 <naweensamaranayake@gmail.com>
2022-11-29 13:52:40 +0530

    todo table is edited

--- src/main/java/lk/ijse/dep9/entity/Todo.java
+++ src/main/java/lk/ijse/dep9/entity/Todo.java
@@ -1,9 +1,12 @@
 package lk.ijse.dep9.entity;
 
 import jakarta.validation.constraints.NotEmpty;
+import lk.ijse.dep9.entity.util.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Stack;
 
 @Data
 @NoArgsConstructor
@@ -13,5 +16,5 @@ public class Todo implements SuperEntity{
     private int id;
     private String username;
     private String description;
-    private String status;
+    private Status status;
 }

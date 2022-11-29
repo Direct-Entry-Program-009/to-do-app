b854cb7ae402e2c5291d79dc99ec6812f2dca426
visalsrimanga <visalsrimanga@gmail.com>
2022-11-29 11:42:58 +0530

    create the todo -item table2

--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -1,1 +1,11 @@
 CREATE TABLE U
+
+
+
+CREATE TABLE todo_item(
+                          id INT AUTO_INCREMENT PRIMARY KEY ,
+                          user_name VARCHAR(50) NOT NULL ,
+                          description VARCHAR(100) NOT NULL ,
+                          status ENUM('DONE', 'NOT DONE'),
+                          CONSTRAINT FOREIGN KEY (user_name) REFERENCES user(user_name)
+);

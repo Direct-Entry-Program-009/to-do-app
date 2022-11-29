1ab935f49f690099c58394cf27a2bbff55edaa63
visalsrimanga <visalsrimanga@gmail.com>
2022-11-29 14:15:45 +0530

    create TodoDTO

--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -11,6 +11,6 @@ CREATE TABLE todo_item(
                           id INT AUTO_INCREMENT PRIMARY KEY ,
                           user_name VARCHAR(50) NOT NULL ,
                           description VARCHAR(100) NOT NULL ,
-                          status ENUM('DONE', 'NOT DONE'),
+                          status ENUM('DONE', 'NOT_DONE'),
                           CONSTRAINT FOREIGN KEY (user_name) REFERENCES user(user_name)
 );

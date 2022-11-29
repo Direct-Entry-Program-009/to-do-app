652a1dec9465cfda73c5b97789ec30628d516c0a
visalsrimanga <visalsrimanga@gmail.com>
2022-11-29 12:24:00 +0530

    take user servelet

--- src/main/java/lk/ijse/dep9/api/TodoServlet.java
+++ src/main/java/lk/ijse/dep9/api/TodoServlet.java
@@ -3,11 +3,12 @@ package lk.ijse.dep9.api;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.ijse.dep9.api.util.HttpServlet2;
 
 import java.io.IOException;
 
 @WebServlet(name = "TodoServlet", value = "/todo")
-public class TodoServlet extends HttpServlet {
+public class TodoServlet extends HttpServlet2 {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 

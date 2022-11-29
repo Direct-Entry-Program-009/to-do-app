283caece83250fb00e192cd87a99986dcfd035e1
naween1994 <naweensamaranayake@gmail.com>
2022-11-29 14:17:20 +0530

    user dto is created

--- src/main/java/lk/ijse/dep9/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/dto/UserDTO.java
@@ -1,4 +1,23 @@
 package lk.ijse.dep9.dto;
 
-public class UserDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO implements Serializable {
+    @NotBlank(message = "User name cannot be blank")
+    @Pattern(regexp = "[A-Za-z0-9@_ ]+",message = "Invalid user name. '. + * ? ^ $ ( ) [ ] { } |' cannot be used ")
+    private String userName;
+    @NotBlank(message = "Full name cannot be blank")
+    @Pattern(regexp = "[A-Za-z ]+",message = "Invalid Full Name")
+    private String fullName;
+    @NotBlank(message = "Password Cannot be Blank")
+    private String password;
 }

6aaa37e1545cd6e8734aa10bfe2ca88b35a685ac
visalsrimanga <visalsrimanga@gmail.com>
2022-11-29 12:20:38 +0530

    crete doPost

---
+++ src/main/java/lk/ijse/dep9/api/TodoServlet.java
@@ -0,0 +1,39 @@
+package lk.ijse.dep9.api;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "TodoServlet", value = "/todo")
+public class TodoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getPathInfo() == null || request.getPathInfo().equals("/")){
+            try{
+                if (request.getContentType() == null || !request.getContentType().startsWith("application/json")){
+                    throw new RuntimeException("Invalid JSON");
+                }
+
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_NO_CONTENT);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+
+}
